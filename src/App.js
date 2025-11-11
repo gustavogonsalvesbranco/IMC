bffb3243e4984e40e333d3d4141e6ea059f8ff56
gustavogonsalvesbranco <dollop.velocity.defendant>
2025-11-11 14:00:41 -0300

    :art: style: Aplicado a estilização da tela

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import "./app.css";
+
 export default function App() {
   return (
     <div className="app">
       <h1>Calculadora IMC</h1>
+
       <span>Vamos calcular seu imc</span>
-      <div className="area-input">
+      <div className="areaInput">
         <input type="text" placeholder="Peso em (kg) Ex: 80" />
         <input type="text" placeholder="Altura em (cm) Ex: 176" />
         <button>Calcular</button>
       </div>
 
-      <h2>Seu imc foi 25, você está a cima do peso</h2>
+      <h2>Seu imc foi 25, você está abaixo do peso</h2>
     </div>
   );
 }
